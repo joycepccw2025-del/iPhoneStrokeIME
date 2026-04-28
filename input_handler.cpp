3c901601a81d9ef4945820a4a8d5437d91eed333
joycepccw2025-del <joycepccw2025@gmail.com>
2026-04-28 17:30:58 +0800

    Update input_handler.cpp

--- input_handler.cpp
+++ input_handler.cpp
@@ -1,21 +1,8 @@
 #include "input_handler.h"
 #include "ime_core.h"
 #include "dictionary.h"
-#include <windows.h>
 
 extern GlobalState g_state;
-
-void InputHandler::sendTextDirectUnicode(const std::wstring& text) {
-    for (wchar_t c : text) {
-        INPUT input[2] = {0};
-        input[0].type = INPUT_KEYBOARD;
-        input[0].ki.wScan = c;
-        input[0].ki.dwFlags = KEYEVENTF_UNICODE;
-        input[1] = input[0];
-        input[1].ki.dwFlags |= KEYEVENTF_KEYUP;
-        SendInput(2, input, sizeof(INPUT));
-    }
-}
 
 LRESULT CALLBACK InputHandler::KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode == HC_ACTION && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)) {
@@ -35,14 +22,16 @@ LRESULT CALLBACK InputHandler::KeyboardHookProc(int nCode, WPARAM wParam, LPARAM
             if (stroke) {
                 g_state.inputBuffer += stroke;
                 Dictionary::updateCandidates(g_state);
+                InvalidateRect(g_state.hWnd, NULL, TRUE);
                 return 1;
             }
         }
         
-        // Numpad 1,2,3 選字
+        // 選字 (Numpad 1,2,3)
         if (pKey->vkCode >= VK_NUMPAD1 && pKey->vkCode <= VK_NUMPAD3) {
             if (!g_state.candidates.empty()) {
                 Dictionary::selectCandidate(g_state, pKey->vkCode - VK_NUMPAD1);
+                InvalidateRect(g_state.hWnd, NULL, TRUE);
                 return 1;
             }
         }
@@ -50,6 +39,7 @@ LRESULT CALLBACK InputHandler::KeyboardHookProc(int nCode, WPARAM wParam, LPARAM
         if (pKey->vkCode == VK_BACK && !g_state.inputBuffer.empty()) {
             g_state.inputBuffer.pop_back();
             Dictionary::updateCandidates(g_state);
+            InvalidateRect(g_state.hWnd, NULL, TRUE);
             return 1;
         }
     }
