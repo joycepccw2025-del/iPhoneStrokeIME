6b2b4a6ec90101779864dec018897c5b1bb312a6
joycepccw2025-del <joycepccw2025@gmail.com>
2026-04-28 17:42:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,20 @@
 #include "dictionary.h"
 
 GlobalState g_state;
-HHOOK g_hKeyboardHook = NULL;
+HHOOK g_hHook = NULL;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow) {
     g_state.hInstance = hInst;
-    
-    wchar_t path[MAX_PATH];
-    GetModuleFileNameW(NULL, path, MAX_PATH);
-    std::wstring dir = path;
-    g_state.systemDir = dir.substr(0, dir.find_last_of(L"\\/") + 1);
+    wchar_t p[MAX_PATH];
+    GetModuleFileNameW(NULL, p, MAX_PATH);
+    std::wstring d = p;
+    g_state.systemDir = d.substr(0, d.find_last_of(L"\\/") + 1);
 
     Dictionary::loadMainDict(g_state);
     WindowManager::registerOptimizedWindowClasses(hInst);
     WindowManager::createOptimizedWindows(hInst, g_state);
 
-    g_hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, InputHandler::KeyboardHookProc, hInst, 0);
+    g_hHook = SetWindowsHookEx(WH_KEYBOARD_LL, InputHandler::KeyboardHookProc, hInst, 0);
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
